b8167b5d7ab7e8878276808915d91bd571b8b416
Dan Dumitrache <vietnamezul@gmail.com>
2018-04-16 10:50:20 +0000

    email form works locally, display the message within the terminal, see settings/local.py

--- project3/settings/local.py
+++ project3/settings/local.py
@@ -7,3 +7,11 @@ STATIC_ROOT = 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
+
+# Testing email form on contact page
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
