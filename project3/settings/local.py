6bd4029a42e2240f5ccb41a67706bde584d03970
Dan Dumitrache <vietnamezul@gmail.com>
2018-04-17 06:47:17 +0000

    modified bashrc email host user

--- project3/settings/local.py
+++ project3/settings/local.py
@@ -9,9 +9,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
 
 # Testing email form on contact page
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_USE_TLS = True       
-EMAIL_HOST = 'smtp.sendgrid.net'      
-EMAIL_PORT = 587     
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')     
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_USE_TLS = True       
+# EMAIL_HOST = 'smtp.sendgrid.net'      
+# EMAIL_PORT = 587     
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')     
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
