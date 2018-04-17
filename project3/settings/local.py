b64cbb1c14260d4b0f110493458a2a864d03d088
Dan Dumitrache <vietnamezul@gmail.com>
2018-04-17 06:35:40 +0000

    trying to integrate sendgrid

--- project3/settings/local.py
+++ project3/settings/local.py
@@ -10,8 +10,8 @@ MEDIA_ROOT = 'media'
 
 # Testing email form on contact page
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'testing@example.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
-EMAIL_PORT = 1025
+EMAIL_USE_TLS = True       
+EMAIL_HOST = 'smtp.sendgrid.net'      
+EMAIL_PORT = 587     
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')     
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
